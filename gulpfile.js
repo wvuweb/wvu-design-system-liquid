3ca95bf4d9365876978cdd93c2f966ce225d7c1c
Adam Glenn <adamglenn@Adams-MacBook-Pro.local>
2019-03-28 08:36:22 -0400

    Removing gulp-concat from gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var concat = require('gulp-concat');
 
 // Sass task
 // Compile Our Sass from the "scss" directory
@@ -14,10 +13,4 @@ gulp.task('sass', function () {
 
 gulp.task('default', ['sass'], function () {
   gulp.watch(['scss/**/*.scss'], ['sass']);
-});
-
-gulp.task('scripts', function() {
-  return gulp.src(['./javascripts/vendor/responsive-nav-dropdowns.js', './javascripts/responsive-nav-dropdown--custom.js', './javascripts/vendor/jquery.magnific-popus.js', './javascripts/vendor/twitterFetcher.js'])
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./javascripts/'));
 });
